1273918a07b2a9ea48430d84a93e685090afe935
molive <miguel.oliva@mulesoft.com>
2015-11-02 14:02:41 -0300

    Changing studioproductpath and studioproductini to 'private' functions (starting with __)

--- studio.sh
+++ studio.sh
@@ -2,6 +2,8 @@
 PATH_TO_PRODUCT=org.mule.tooling.products/org.mule.tooling.studio.product/target/products/studio.product/macosx/cocoa/x86_64/AnypointStudio
 STUDIO_EXEC_NAME=AnypointStudio
 
+DEBUG_LINE="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005"
+
 function __studioproductpathfromcontext {
 	if [ -z $1 ]; then return 1; else
 	       	if [ $1 = "/" ]; then __path="Error trying to find path from this context"; else
@@ -14,35 +16,33 @@ function __studioproductpathfromcontext {
 	echo $__path
 }
 
-function studioproductpath {
+function __studioproductpath {
 	if [ -z $1 ]; then REPO_NAME=$(__studioproductpathfromcontext `pwd`); else REPO_NAME=$WS_HOME/$1; fi
 	echo $REPO_NAME/$PATH_TO_PRODUCT
 }
 
-function studioproductini {
-	local PRODUCT_PATH=$(studioproductpath $1)
+function __studioproductini {
+	local PRODUCT_PATH=$(__studioproductpath $1)
 	echo $PRODUCT_PATH/$STUDIO_EXEC_NAME.app/Contents/MacOS/$STUDIO_EXEC_NAME.ini
 }
 
 function editstudioproductini {
-	local ini_file=$(studioproductini $1)
+	local ini_file=$(__studioproductini $1)
 	edit $ini_file
 }
 
 function openstudio {
-	local PRODUCT_PATH=$(studioproductpath $1)
+	local PRODUCT_PATH=$(__studioproductpath $1)
 	open $PRODUCT_PATH/$STUDIO_EXEC_NAME
 }
 
-DEBUG_LINE="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005"
-
 function setdebugforstudio {
-	local STUDIO_INI=$(studioproductini $1)
+	local STUDIO_INI=$(__studioproductini $1)
 	echo $DEBUG_LINE >> $STUDIO_INI
 }
 
 function unsetdebugforstudio {
-	local STUDIO_INI=$(studioproductini $1)
+	local STUDIO_INI=$(__studioproductini $1)
 	sed -i.ini "/^$DEBUG_LINE/d" $STUDIO_INI
 }
 
